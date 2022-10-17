54e541208f5cc1633454385c4fd7c3b6514ada93
thisd92 <thiiagodutra@gmail.com>
2022-10-17 13:57:24 -0300

    Frontend and Backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div id="app">
+        <h1>Base de Conhecimento!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+}
+</script>
+
+<style>
+
+</style>
